955d1286ae5e416ac3a1598086f80f6b896033d5
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-20 22:14:59 +0430

    Null Coalesce Operator

--- index.php
+++ index.php
@@ -11,6 +11,17 @@ declare(strict_types=1);
 //int bool float
 function sum(float $a , float $b):float {
     return $a + $b;
+
 }
 
 var_dump(sum(1,2));
+
+//old
+$username = isset($_GET['user']) ? $_GET['user'] : 'nobody';
+
+//new
+$username = $_GET['user'] ?? 'nobody';
+//or
+$username = $_GET['user'] ?? $_POST['user'] ?? 'nobody';
+
+var_dump($username);
